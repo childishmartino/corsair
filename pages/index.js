b52f60b2f07fdab903fabfe4dc7708a1035fd25c
Chris <chrisg1821@gmail.com>
2021-11-21 16:14:06 -0600

    background images and sections set

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/*Nav needs to be sticky*/}
       <nav class="bg-black">
         <div class="max-w-6xl mx-auto px-4">
           <div class="flex justify-between">
@@ -19,12 +20,14 @@ export default function Home() {
                 <a href="#" class="py-5 px-3 text-white hover:text-gray-900">Panel 4</a>
                 <a href="#" class="py-5 px-3 text-white hover:text-gray-900">Panel 5</a>
               </div>
+              {/*Mobile Nav?*/}
             </div>
           </div>
         </div>
       </nav>
 
-      <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+      {/*looping background*/}
+      <header class="relative flex items-center justify-center h-screen overflow-hidden">
         <div class="relative z-30 p-5">
           <h2 class="text-yellow-300">MAKE THE JUMP TO HYPERSPEED</h2>
           <h1 class="text-2xl text-white">CORSAIR MEMORY</h1>
@@ -35,6 +38,26 @@ export default function Home() {
           Your browser does not support the video tag.
         </video>
       </header>
+
+      {/*Section 1 backgound image, carousel buttons (left & right) and progress idicator (1-6)*/}
+      <section class="w-full h-screen bg-iCue bg-cover bg-center">
+      </section>
+
+      {/*Section 2 background image, animated image sequence controlled by scroll, scrolling back up reverses sequence*/}
+      <section class="w-full h-screen bg-hex bg-cover bg-center">
+      </section>
+
+      {/*Section 3 5 videos that change with the swatches*/}
+      <section class="w-full h-screen bg-black bg-cover bg-center">
+      </section>
+
+      {/*Section 4 video once, fade out and animate 4 keyboards sliding in from bottom*/}
+      <section class="w-full h-screen bg-flavor bg-cover bg-center">
+      </section>
+
+      {/*Section 5 black background, data loaded from JSON URL, drop down needs 550, 650 and 750 watt options (specs change/load)*/}
+      <section class="w-full h-screen bg-black bg-cover bg-center">
+      </section>
           
 
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        iCue: "url('/iCue-panel-bg.jpg')",
+        hex: "url('/bg-TriHexBackground3.png')",
+        flavor: "url('/bg-creme-min.png')",
+      }),
+    },
   },
   variants: {
     extend: {},
